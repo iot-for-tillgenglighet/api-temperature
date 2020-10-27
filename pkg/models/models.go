7d52e800cf72204dfa23f94d9a1d3a2090392fc0
Isak Styf <isak.styf@masarinconsulting.se>
2020-10-27 18:40:44 +0100

    Added database index on device ID

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -8,7 +8,7 @@ type Temperature struct {
 	gorm.Model
 	Latitude  float64
 	Longitude float64
-	Device    string `gorm:"unique_index:idx_device_timestamp"`
+	Device    string `gorm:"unique_index:idx_device_timestamp;index:idx_device_temp"`
 	Temp      float32
 	Water     bool
 	Timestamp string `gorm:"unique_index:idx_device_timestamp"`
